af6473aa3772a38ff9161cac3b0a5dc2bc4fb9ab
Tomas <tomas_federal@hotmail.com>
2019-06-06 20:02:01 -0300

    mini Fix

--- tp01/src/main/java/utn/frsf/mst/aepad/tp01/service/PedidoService.java
+++ tp01/src/main/java/utn/frsf/mst/aepad/tp01/service/PedidoService.java
@@ -1,4 +1,17 @@
 package utn.frsf.mst.aepad.tp01.service;
 
+import utn.frsf.mst.aepad.tp01.MODELO.Pedido;
+
+import java.util.List;
+
 public interface PedidoService {
+
+    Pedido buscarPorId(Integer id);
+
+    Pedido guardar(Pedido t);
+
+    void borrar(Integer id);
+
+    List<Pedido> buscarTodas();
+
 }
